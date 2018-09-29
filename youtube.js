d5e9987c4a0dfe7b612e01f2260c5e958265a1b5
thisisbrix901 <bmadeira7@gmail.com>
2018-09-29 01:53:23 -0400

    found out which songs were working, and which ones were not

--- youtube.js
+++ youtube.js
@@ -1,4 +1,4 @@
-//ajax call to Last.Fm api // this might not be in our final submission, but keeping this code for now.
+// // ajax call to Last.Fm api // this might not be in our final submission, but keeping this code for now.
 
 // var artist = "Bob Marley"
 // function searchLastFM(artist) {
@@ -17,9 +17,8 @@
 // searchLastFM(artist)
 
 
-//this code shows a visual table of the youtube ajax response data
 
-var apiKey = "AIzaSyCidNiMCQoVdkF4cEvUdGbhTut0WujY9D8";
+
 
 
 /////////////////////this code is for the embedded iFrame player
@@ -27,6 +26,7 @@ var apiKey = "AIzaSyCidNiMCQoVdkF4cEvUdGbhTut0WujY9D8";
 // 2. This code loads the IFrame Player API code asynchronously.
 var tag = document.createElement('script');
 
+var apiKey = "AIzaSyCidNiMCQoVdkF4cEvUdGbhTut0WujY9D8";
 tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
@@ -38,8 +38,8 @@ function onYouTubeIframeAPIReady() {
     player = new YT.Player('player', {
         height: '390',
         width: '640',
-        //this part of code determines which video we wanna play.
-        videoId: 'Va_RU-tph-Y',
+        //this part of code determines which video we want to play.
+        videoId: 'tCnBrrnOefs',
         ///////////////////////////////////////////////////////
         events: {
             'onReady': onPlayerReady,
@@ -59,7 +59,7 @@ function onPlayerReady(event) {
 var done = false;
 function onPlayerStateChange(event) {
     if (event.data == YT.PlayerState.PLAYING && !done) {
-        setTimeout(stopVideo, 60000);
+        setTimeout(stopVideo, 600000);
         done = true;
     }
 }
@@ -67,35 +67,80 @@ function stopVideo() {
     player.stopVideo();
 }
 
-
-var sadness = ["nZq_jeYsbTs","y9Gf-f_hWpU","Va_RU-tph-Y","X--KAhRMaQY","RB-RcX5DS5A","eYUPa2X4aoc","Na2fIC3CXYE","S9bCLPwzSC0","pgN-vvVVxMA","4N3N1MlvVc4"]
+// MUSIC ARRAYS// 
+//FORMAT: ARTIST "SONG TITLE" - VIDEOID  ----- whether it works when embedded
+//-------------------------------------
+function pickSong() {
+var sadness = ["nZq_jeYsbTs", "Va_RU-tph-Y", "Na2fIC3CXYE",  "4N3N1MlvVc4", "LLwRLC2AB3M", "D_P-v1BVQn8", "Yw9l9jLsyiU" ];
+var randomSadSong = sadness[Math.floor(Math.random() * ((sadness.length + 1)))];
+console.log(randomSadSong)
 //sadness tunes////////////////////////////////////////
-//Radiohead "How To Disappear Completely" - nZq_jeYsbTs
-//The Smiths "There Is A Light That Never Goes Out" - y9Gf-f_hWpU
-//The Anniversary "All Things Ordinary" - Va_RU-tph-Y
-//Joji "Slow Dancing In The Dark" - X--KAhRMaQY
-//Coldplay "The Scientist" - RB-RcX5DS5A
-//Tame Impala "The Less I Know The Better" - eYUPa2X4aoc
-//nofx "My Orphan Year (Acoustic)" - Na2fIC3CXYE
-//Eminem "Mockingbird"- S9bCLPwzSC0
-//XXXTENTACION "SAD!"- pgN-vvVVxMA
-//Gary Jules "Mad World" - 4N3N1MlvVc4
+//Radiohead "How To Disappear Completely" - nZq_jeYsbTs     -----WORKS
+//The Anniversary "All Things Ordinary" - Va_RU-tph-Y       -----WORKS
+//nofx "My Orphan Year (Acoustic)" - Na2fIC3CXYE            -----WORKS
+//Gary Jules "Mad World" - 4N3N1MlvVc4                      -----WORKS
+//Gemini "Blue" - LLwRLC2AB3M                               -----WORKS
+//Gilbert O'Sullivan "Alone Again(Naturally)" - D_P-v1BVQn8 -----WORKS
+//Joe Harnell "The Lonely Man" - Yw9l9jLsyiU                -----WORKS
 
-var happiness = []
+//XXXTENTACION "SAD!"- pgN-vvVVxMA                                 ------ //NOT WORKING!!!!
+//Eminem "Mockingbird"- S9bCLPwzSC0                               --------//NOT WORKING!!!!
+//Tame Impala "The Less I Know The Better" - eYUPa2X4aoc          --------//NOT WORKING!!!!
+//Joji "Slow Dancing In The Dark" - X--KAhRMaQY                   --------//NOT WORKING!!!!
+//Coldplay "The Scientist" - RB-RcX5DS5A                          --------//NOT WORKING!!!!
+//The Smiths "There Is A Light That Never Goes Out" - y9Gf-f_hWpU --------//NOT WORKING!!!!
+
+var happiness = ["tCnBrrnOefs", "P68J72bsSNk", "mMfxI3r_LyA","xumGNwplds8", "GHUc6juS0YQ"];
+var randomhappySong = happiness[Math.floor(Math.random() * ((happiness.length + 1)))];
+console.log(randomhappySong)
+//HAPPINESS TUNES /////////////////////////////////////
+// Justice "D.A.N.C.E." - tCnBrrnOefs                   -----WORKS
+//Guttermouth "Perfect World" -P68J72bsSNk              -----WORKS
+//Modjo "Lady(Hear Me Tonight)" - mMfxI3r_LyA           -----WORKS
+//Stardust "Music Sounds Better With You" - xumGNwplds8 -----WORKS
+//James Brown "I Feel Good(I Got You)" - GHUc6juS0YQ    -----WORKS
 
 
 
-var anger = []
+var anger = ["vlXi129C39M", "CCC8vCxEXQo", "0ZE1bmcWMUY", "PBWq1OtN7Cc", "RD_kgKyDk6c", "R8fLOJswWtk", "ZrFTR9fucr8", "Wh-Jf_udRhs"]
+//ANGER TUNES//////////////////////////////////////////
+var randomAngrySong = anger[Math.floor(Math.random() * ((anger.length + 1)))];
+console.log(randomAngrySong)
+//Noisia & Evol Intent "Long Gone" - vlXi129C39M                 -----WORKS
+//Trampa & Cookie Monsta "Move The Crowd" - CCC8vCxEXQo          -----WORKS
+//August Burns Red "Meddler" - 0ZE1bmcWMUY                       -----WORKS
+//Hatebreed "Conceived Through An Act of Violence" - PBWq1OtN7Cc -----WORKS
+//Ramones "Ignorance is Bliss" - RD_kgKyDk6c                     -----WORKS
+//Sex Pistols "God Save The Queen" - R8fLOJswWtk                ------WORKS
+//I Wrestled A Bear Once "Smells Like Kevin Bacon" - ZrFTR9fucr8 -----WORKS
+//Funtcase "Scary Yikes Grrz" - Wh-Jf_udRhs                      -----WORKS
 
-var neutral = ["Kp7eSUU9oy8","6zf6yce6g7g","yvK3t0TdYfQ","_Rf-pixOCEQ"]
+ 
+
+
+var neutral = ["6zf6yce6g7g", "yvK3t0TdYfQ", "A5o4fn5imNw", "zfcHq0hhFWg", "EAEKK8xCpBY", "_FrOQC-zEog", "Pz1W1OLkw14", "3Q9rewnLFYw", "slldMEPvUqA", "0KJHH08FPpE", "_BObK59njSg"]
 //NEUTRAL TUNES////////////////////////////////////////
-//childish gambino "redbone" - Kp7eSUU9oy8
-//Killedmyself "are you lost?" - 6zf6yce6g7g
-//Morning High "Sativa Satva" - yvK3t0TdYfQ
-//Nosaj Thing "Realize" - _Rf-pixOCEQ
-//
-//
-//
-//
-//
-//
+//Killedmyself "are you lost?" - 6zf6yce6g7g                    -----WORKS
+//Morning High "Sativa Satva" - yvK3t0TdYfQ                     -----WORKS
+//Shogondo "I See What U Mean" - A5o4fn5imNw                    -----WORKS
+//Tame Impala "Half Full Glass Of Wine" - zfcHq0hhFWg           -----WORKS
+//Dispatch "The General" - EAEKK8xCpBY                          -----WORKS
+//Pink Floyd "Comfortably Numb" - _FrOQC-zEog                   -----WORKS
+// High Contrast "If We Ever" - Pz1W1OLkw14                    ------WORKS
+//Flux Pavillion "I Can't Stop" - 3Q9rewnLFYw                  ------WORKS
+//The Whispers "And The Beat Goes On" - slldMEPvUqA            ------WORKS
+//Volac "Open Your Mind" - 0KJHH08FPpE                         ------WORKS
+//Washed Out "Feel It All Around"  - _BObK59njSg                -----WORKS
+
+//Our Lady Peace "Clumsy" - xp2P6JKc1QE           --------//NOT WORKING!!!
+//The Smashing Pumpkins "Today" - zAgvyL9-usc     --------//NOT WORKING!!!
+//Chet Faker "Gold" - hi4pzKvuEQM                  ------ //NOT WORKING!!!
+//Bob Marley "Smile Jamaica" - MAdoRNiepPA         ------ //NOT WORKING!!!
+//M83 "Midnight City" - dX3k_QDnzHE                ------ //NOT WORKING!!!
+//Mt. Joy "Astrovan" - etrjS8dYeFc                 ------ //NOT WORKING!!!
+//Coldplay "Adventure Of A Lifetime" - QtXby3twMmI ------ //NOT WORKING!!!
+//Nosaj Thing "Realize" - _Rf-pixOCEQ              ------ //NOT WORKING!!!
+//childish gambino "redbone" - Kp7eSUU9oy8         ------ //NOT WORKING!!!
+}
+
+pickSong()
